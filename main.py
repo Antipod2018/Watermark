9fcf3c05858f01882affa836a5aa1b37135c1484
Valentin_Mouse39 <47462272+Antipod2018@users.noreply.github.com>
2022-05-21 10:52:24 +0200

    теперь в папке MNB00XXXX, на фотках тоже ставится водяной знак и добавляется индекс '-1'

--- main.py
+++ main.py
@@ -21,12 +21,41 @@ def watermark_text(input_image_path,
 
     photo.save(output_image_path)
 
+def watermark_text2(input_image_path,
+                   output_image_path,
+                   text, image_path, color):
+    photo = Image.open(input_image_path)
+    drawing = ImageDraw.Draw(photo)
+    black = color
+    size=80
+
+    font = ImageFont.truetype("arial.ttf", size)
+    drawing.text((photo.width-len(text)*50, photo.height-size), text, fill=black, font=font)
+
+    t = output_image_path.split('\\')
+    if image_path:
+        image_path = os.path.join(image_path, t[-1])
+        photo.save(image_path)
+    output_image_path = output_image_path[:-4] + '-1.jpg'
+
+    photo.save(output_image_path)
 
 
 
 def InFolder(rootDir, watermark, image_path, text):
     for lists in os.listdir(rootDir):
         path = os.path.join(rootDir, lists)
+
+
+        if path[len(path) - 4:] == '.jpg':
+            if len(watermark) == 9:
+                watermark_text2(path, path,
+                               text=watermark[-4:],
+                               image_path=image_path, color=text)
+            if len(watermark) == 11:
+                watermark_text2(path, path,
+                               text=watermark[-6:],
+                               image_path=image_path, color=text)
         if os.path.isdir(path):
             for list in os.listdir(path):
                 path1 = os.path.join(path, list)
